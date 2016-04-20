fd323123bd87a0a134eff2ec05a8f33b64557e17
shenlin <18710600256@163.com>
2016-04-20 21:41:00 +0800

    用定位实现瀑布流
    每一栏的宽度相同，一共4栏。没写后台，暂时不能实现页面滚动到瀑布流下方时，通过Ajax动态加载下一批图片，但是AJax部分写好了

---
+++ 瀑布流/data.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+{
+	code : 0,
+	list : [
+		{src:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'],title:['这是第1张图片','这是第2张图片','这是第3张图片','这是第4张图片']},
+		{src:['img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg'],title:['这是第5张图片','这是第6张图片','这是第7张图片','这是第8张图片']},
+		{src:['img/9.jpg','img/10.jpg','img/11.jpg','img/12.jpg'],title:['这是第9张图片','这是第10张图片','这是第11张图片','这是第12张图片']},
+		{src:['img/13.jpg','img/14.jpg','img/15.jpg','img/16.jpg'],title:['这是第13张图片','这是第14张图片','这是第15张图片','这是第16张图片']},
+	]
+}
